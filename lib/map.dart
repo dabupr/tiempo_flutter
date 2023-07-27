e1771c8589a35ffdbf47b75f2b8f39f22a405eb1
David <dabupr@outlook.com>
2023-07-27 02:00:34 +0200

    Small fixes

--- lib/map.dart
+++ lib/map.dart
@@ -2,13 +2,16 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
 
-Future<GeoPoint?> mapClicked(BuildContext context,double defaultLatitude, double defaultLongitude) async {
-  ValueNotifier<GeoPoint?> notifier = ValueNotifier(null);
+Future<GeoPoint?> mapClicked(
+    BuildContext context, double defaultLatitude, double defaultLongitude) async {
+  //ValueNotifier<GeoPoint?> notifier = ValueNotifier(null);
 
   var pickedPosition = await showSimplePickerLocation(
     context: context,
+    //title: " ",
     isDismissible: true,
-    textConfirmPicker: "pick",
+    textConfirmPicker: "Pick",
+    contentPadding: const EdgeInsets.all(12),
     initZoom: 7,
     initPosition: GeoPoint(
       latitude: defaultLatitude,
